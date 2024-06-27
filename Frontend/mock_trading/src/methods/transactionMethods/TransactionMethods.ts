35e233db8462fe982339d23b15a8824dbc87fff4
WillssEdgar <willssedgar2123@gmail.com>
2024-06-27 19:51:53 -0400

    feat: pie chart

--- Frontend/mock_trading/src/methods/transactionMethods/TransactionMethods.ts
+++ Frontend/mock_trading/src/methods/transactionMethods/TransactionMethods.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { useState } from "react";
 
-export async function getPortfolio(
-  email: string
-) {
+export async function getPortfolio(email: string) {
   try {
-    const response = await axios.get(`http://localhost:8080/portfolio/${email}`);
-    console.log("Response", response)
+    const response = await axios.get(
+      `http://localhost:8080/portfolio/${email}`,
+    );
+    console.log("Response", response);
     return response.data;
   } catch (error) {
     console.error("error fetching portfolio", error);
@@ -14,35 +14,28 @@ export async function getPortfolio(
 }
 
 interface Stock {
-  Id: number,
-  Symbol: string,
-  PurchaseDate: string,
-  PurchasePrice: number,
-  AmountOfShares: number,
-  PortfolioID: number
+  Id: number;
+  Symbol: string;
+  PurchaseDate: string;
+  PurchasePrice: number;
+  AmountOfShares: number;
+  PortfolioID: number;
 }
 
 interface List {
-  stockList: Stock[]
+  stockList: Stock[];
 }
 
-
-
-export async function getStock(
-  email: string,
-  setStock: (data: any) => void,
-
-) {
-
+export async function getStock(email: string, setStock: (data: any) => void) {
   try {
     const portfolio = await getPortfolio(email);
     const portfolioID = portfolio.ID;
-    const response = await axios.get(`http://localhost:8080/getStocks/${portfolioID}`);
+    const response = await axios.get(
+      `http://localhost:8080/getStocks/${portfolioID}`,
+    );
 
-    console.log("Response of Stock List", response.data);
+    console.log("Response of Stock List In Get Stock", response.data);
     setStock(response.data);
-
-
   } catch (error) {
     console.error("Error", error);
   }
--- Frontend/mock_trading/src/pages/dashboard/Dashboard.tsx
+++ Frontend/mock_trading/src/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,12 @@
-
-// Import necessary libraries and components
-import { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import LineChart from '../../components/Charts/LineChart';
-import { fetchPortfolioInfo } from '../../methods/StockMethods';
-import Card from '../../components/Cards/Card';
-import PieChart from '../../components/Charts/PieChart';
-import { getStockList } from '../../methods/StockList';
-import { getStock } from '../../methods/transactionMethods/TransactionMethods';
-import TransactionCard from '../../components/Cards/TransactionCard';
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import LineChart from "../../components/Charts/LineChart";
+import { fetchPortfolioInfo } from "../../methods/StockMethods";
+import Card from "../../components/Cards/Card";
+import PieChart from "../../components/Charts/PieChart";
+import { getStockList } from "../../methods/StockList";
+import { getStock } from "../../methods/transactionMethods/TransactionMethods";
+import TransactionCard from "../../components/Cards/TransactionCard";
 
 interface CompanyInfo {
   message: any;
@@ -25,12 +23,12 @@ interface Stock {
 }
 
 interface OwnedStock {
-  AmountOfShares: number
-  Id: number
-  PortfolioID: number
-  PurchaseDate: string
-  PurchasePrice: number
-  Symbol: string
+  AmountOfShares: number;
+  Id: number;
+  PortfolioID: number;
+  PurchaseDate: string;
+  PurchasePrice: number;
+  Symbol: string;
 }
 
 type OwnedStockList = OwnedStock[];
@@ -40,17 +38,15 @@ interface StockList {
 }
 
 interface PieData {
-  labels: string[],
-  values: number[],
-
+  labels: string[];
+  values: number[];
 }
 
 const Dashboard = () => {
-
   const location = useLocation();
   const navigate = useNavigate();
-  const email = location.state.email;
-  console.log('Email', email);
+  const email = location.state?.email;
+
   if (!email) {
     navigate("/");
   }
@@ -60,119 +56,88 @@ const Dashboard = () => {
   const [data, setData] = useState<FinancialData>({ dates: [], value: [] });
   const [stockList, setStockList] = useState<StockList | null>(null);
   const [stock, setStock] = useState<OwnedStockList | null>(null);
+  const [pieData, setPieData] = useState<PieData | null>(null);
 
   useEffect(() => {
-
     const fetchPortfolioData = async () => {
       try {
         await fetchPortfolioInfo(email, setInfo, setInfoError);
       } catch (error) {
-        setInfoError('Error fetching company info');
+        setInfoError("Error fetching company info");
+      }
+    };
+
+    const fetchStockList = async () => {
+      try {
+        await getStockList(setStockList);
+        console.log("THis is the stock list in dashboard: ", stockList);
+      } catch (error) {
+        console.log("Error", error);
+      }
+    };
+
+    const fetchStock = async () => {
+      try {
+        await getStock(email, setStock);
+      } catch (error) {
+        console.log("Error fetching stock", error);
       }
     };
 
     if (email) {
       fetchPortfolioData();
+      fetchStockList();
+      fetchStock();
     }
   }, [email]);
 
   useEffect(() => {
-    const fetchStockList = async () => {
-      try {
-        await getStockList(setStockList);
-      } catch (error) {
-        console.log("Error", error);
-      }
-    }
-    if (!stockList) {
-      fetchStockList();
-    }
+    const updatePieData = () => {
+      const PieMap = new Map<string, number>();
 
-  });
-  useEffect(() => {
-    const fetchStock = async () => {
-      if (email) {
+      if (stock) {
+        for (let i = 0; i < stock.length; i++) {
+          const amount = stock[i].AmountOfShares;
+          const newSymbol = stock[i].Symbol;
 
-        await getStock(email, setStock);
+          if (PieMap.has(newSymbol)) {
+            const existingAmount = PieMap.get(newSymbol) ?? 0;
+            const newAmount = existingAmount + amount;
 
+            PieMap.set(newSymbol, newAmount);
+          } else {
+            PieMap.set(newSymbol, amount);
+          }
+        }
+
+        setPieData({
+          labels: Array.from(PieMap.keys()),
+          values: Array.from(PieMap.values()),
+        });
       }
     };
-    fetchStock();
-    console.log("Stock", stock)
-  }, [email]);
-  const pieData = {
-    labels: [],
-    values: [],
-  };
 
-  useEffect(() => {
-    const PieMap = new Map<string, number>();
-
-    if (stock) {
-      for (let i = 0; i < stock.length; i++) {
-        const amount = stock[i].AmountOfShares;
-        const newSymbol = stock[i].Symbol;
-
-        if (PieMap.has(newSymbol)) {
-          // Use optional chaining to access newAmount safely
-          const existingAmount = (PieMap.get(newSymbol) as number) ?? 0;
-          const newAmount = existingAmount + amount;
-
-          PieMap.set(newSymbol, newAmount);
-        } else {
-          PieMap.set(newSymbol, amount);
-        }
-      }
-      pieData.labels = Array.from(PieMap.keys());
-      pieData.values = Array.from(PieMap.values());
-      console.log("Pie Data", pieData);
-    }
-
-    // for (let entry of PieMap.entries()) {
-    //   pieData.labels.push(entry[0]);
-    //   pieData.values.push(entry[1]);
-    // }
-
-  }, [stock])
-
-  // useEffect(() => {
-  //   if (info) {
-  //     const chartData = info.message[0];
-  //     console.log("chartData", chartData);
-  //
-  //     const timestamps = chartData.timestamp;
-  //     console.log("TimeStamps: ", timestamps);
-  //
-  //     const values = chartData.indicators.quote[0].close;
-  //     console.log("Values: ", values);
-  //
-  //     const formattedDates = timestamps.map((timestamp: number) =>
-  //       new Date(timestamp * 1000).toLocaleDateString()
-  //     );
-  //
-  //     setData({ dates: formattedDates, value: values });
-  //     console.log("Data in fetchCompanyData: ", data);
-  //   }
-  // }, [info]);
+    updatePieData();
+  }, [stock]);
 
   const lineData = {
-    dates: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    dates: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
     value: [12, 19, 3, 5, 2, 3],
   };
 
   return (
-    <>
-      <div>
-        <div className="grid grid-cols-5 gap-4">
-          <div className="col-span-4">
-            <div className=" p-4 mb-4 w-full">
-              <div className="flex flex-row gap-2 justify-center items-center">
-                <div className="container mx-auto bg-slate-200 p-4 rounded-lg shadow-lg h-60">
-                  <div className='text-xl'>
-                    Current Balance:      <span className='font-bold'> $1,832,022.23</span>
-                    <div className='scroll-smooth overflow-auto m-4 h-40'>
-                      {stock && stock.map((stockItem, index) => (
-
+    <div>
+      <div className="grid grid-cols-5 gap-4">
+        <div className="col-span-4">
+          <div className="p-4 mb-4 w-full">
+            <div className="flex flex-row gap-2 justify-center items-center">
+              <div className="container mx-auto bg-slate-200 p-4 rounded-lg shadow-lg h-60">
+                <div className="text-xl">
+                  Current Balance:{" "}
+                  <span className="font-bold">$1,832,022.23</span>
+                  <div className="scroll-smooth overflow-auto m-4 h-40">
+                    {stock &&
+                      stock.map((stockItem, index) => (
                         <TransactionCard
                           key={index}
                           stockdata={{
@@ -181,58 +146,51 @@ const Dashboard = () => {
                             PortfolioID: stockItem.PortfolioID,
                             PurchaseDate: stockItem.PurchaseDate,
                             PurchasePrice: stockItem.PurchasePrice,
-                            Symbol: stockItem.Symbol
-
-                          }} />
-
+                            Symbol: stockItem.Symbol,
+                          }}
+                        />
                       ))}
-                    </div>
-                  </div>
-                </div>
-                <div className="container mx-auto bg-slate-200 p-4 rounded-lg shadow-lg h-60">
-                  <div>
-                    <PieChart data={pieData} />
                   </div>
                 </div>
               </div>
-            </div>
-            <div className=" p-10 m-3 w-full">
-              <LineChart data={lineData} />
+              <div className="container mx-auto bg-slate-200 p-4 rounded-lg shadow-lg h-60">
+                <div>
+                  <h1> Pie Chart Data </h1>
+                  <PieChart data={pieData ?? { labels: [], values: [] }} />
+                </div>
+              </div>
             </div>
           </div>
-          <div className=" p-4">
-
-            {stockList?.map((stock: { Symbol: string, Name: string }, index: number) => (
-              <div key={index} className="flex justify-center items-center mb-4">
-                <Card
-                  key={index}
-                  stockdata={{ stocksymbol: stock.Symbol, stockName: stock.Name }}
-                  onClick={() => navigate("/Details", { state: { symbol: stock.Symbol, email: email } })}
-                />
-              </div>
-            ))}
-
+          <div className="p-10 m-3 w-full">
+            <LineChart data={lineData} />
           </div>
         </div>
-      </div>    </>
-    //{/* <div className="min-h-screen flex flex-col justify-center items-center"> */}
-
-    /* <div className="container mx-auto bg-slate-200 p-4 rounded-lg shadow-lg"> */
-    /*   <div className="w-full flex flex-col justify-center items-center"> */
-    /* <h1 className='text-3xl'>Line Chart Example</h1> */
-    /* {dataError && <p>Error: {dataError}</p>} */
-    /* <div className="chart-container" style={{ width: '80%', height: '60%' }}> */
-    /*   <LineChart data={data} /> */
-    /* </div> */
-    /* </div> */
-
-    /* </div> */
-    /* <div> */
-    /*   <Carousel stockNames={defaultStockNames} email={email} /> */
-    /* </div> */
-    // </div>
+        <div className="p-4">
+          {stockList?.map(
+            (stock: { Symbol: string; Name: string }, index: number) => (
+              <div
+                key={index}
+                className="flex justify-center items-center mb-4"
+              >
+                <Card
+                  key={index}
+                  stockdata={{
+                    stocksymbol: stock.Symbol,
+                    stockName: stock.Name,
+                  }}
+                  onClick={() =>
+                    navigate("/Details", {
+                      state: { symbol: stock.Symbol, email: email },
+                    })
+                  }
+                />
+              </div>
+            ),
+          )}{" "}
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default Dashboard;
-
